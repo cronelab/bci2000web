59e905297f39038fec9efc1a2ea8e726f291dfea
Chris <17508521+TheBrainChain@users.noreply.github.com>
2019-01-10 15:34:14 -0500

    new REST APIs, server structure, and helper functions

---
+++ server/bci2000web.js
@@ -0,0 +1,186 @@
+#!/usr/bin/env/ node
+
+// bci2000web.js
+// A node-based implementation of web-socket based BCI2000 remote control
+
+'use strict'
+const path = require( 'path' );
+const fs = require( 'fs' );
+const express = require( 'express' );
+const app = express();
+const expressWs = require( 'express-ws' )( app );
+const Telnet = require( 'telnet-client' );
+const helpers = require('./helpers.js');
+const config = require('./config.json');
+const opn = require('opn');
+const operatorPath = `${path.resolve(config.bci2kdir)}/prog/Operator.exe`;
+
+  app.get('/paradigms', (req, res) => {
+	const cardPaths = helpers.findCards('./web/paradigms');
+	const cards = cardPaths.map((cardPath) => {
+	  const cardDir = path.dirname(cardPath);
+	  const cardDirParts = cardDir.split(path.sep);
+	  const cardRoot = cardDirParts.slice(cardDirParts.length - 2).join('/');
+	  const cardName = cardDirParts[cardDirParts.length - 1];
+	  return {
+		name: cardName,
+		path: cardPath,
+		root: cardRoot,
+	  };
+	});
+	res.send(cards);
+  });
+
+  app.get("/subjects", (req, res) => {
+		const dataPaths = helpers.findData("./data");
+		function onlyUnique(value, index, self) {
+			return self.indexOf(value) === index;
+		}
+		let subjects = dataPaths
+			.map(x => {
+				return x.split(path.sep).slice(x.split(path.sep).length - 3)[0];
+			})
+			.filter(onlyUnique);
+		res.send(subjects);
+	});
+	app.get("/api/:subj", (req, res) => {
+		let subject = req.params.subj;
+		const dataPaths = helpers.findData(`./data/${subject}`);
+		function onlyUnique(value, index, self) {
+			return self.indexOf(value) === index;
+		}
+		let tasks = dataPaths
+			.map(x => {
+				return x.split(path.sep).slice(x.split(path.sep).length - 2)[0];
+			})
+			.filter(onlyUnique);
+		res.send(tasks);
+	});
+	app.get("/api/:subj/:task", (req, res) => {
+		let subject = req.params.subj;
+		let task = req.params.task;
+		const dataPaths = helpers.findData(`./data/${subject}/${task}`);
+	
+		function onlyUnique(value, index, self) {
+			return self.indexOf(value) === index;
+		}
+		let runs = dataPaths
+			.map(x => {
+				return x.split(path.sep).slice(x.split(path.sep).length - 1)[0];
+			})
+			.filter(onlyUnique);
+		res.send(runs);
+	});
+	  
+  
+
+app.set('views', path.join(__dirname, '../web'))
+
+app.get('/web/index.html', (req, res) => {
+	let cardPaths = helpers.findCards('./server/paradigms');
+	let cards = cardPaths.map(cardPath => {
+		let cardDir = path.dirname( cardPath );
+		let cardDirParts = cardDir.split( path.sep );
+		let cardRoot = cardDirParts.slice( cardDirParts.length - 2).join('/');
+		let cardName = cardDirParts[cardDirParts.length - 1];
+		return {
+			name: cardName,
+			path: cardPath,
+			root: cardRoot
+		};
+	} );
+	res.render( 'index', {cards: cards});
+} );
+
+const connectTelnet = async operator => {
+  const connection = new Telnet();
+  const telnetParams = {
+    host: "127.0.0.1",
+    port: config.telnetPort,
+    timeout: 3000,
+    shellPrompt: ">",
+    echoLines: 0,
+    execTimeout: 30
+  };
+
+  // Cache new parameters in the operator process object
+  operator.telnet = null;
+  operator.commandQueue = [];
+  operator.executing = null;
+
+  connection.on("ready", () => (operator.telnet = connection));
+  connection.on("timeout", () => (operator.executing = null));
+  connection.on("close", () => process.exit(0));
+
+  await connection.connect(telnetParams);
+
+  // Set up WebSocket handler
+  app.ws("/", ws => {
+    ws.on("message", msg => {
+      let preamble = msg.split(" ");
+      var msg = {};
+      msg.opcode = preamble.shift();
+      msg.id = preamble.shift();
+      msg.contents = preamble.join(" ");
+      msg.ws = ws;
+      if (msg.opcode == "E") {
+        operator.commandQueue.push(msg);
+      }
+    });
+  });
+
+  // Start command execution loop
+  (function syncCommunications() {
+    if (
+      operator.commandQueue.length &&
+      operator.telnet &&
+      !operator.executing
+    ) {
+      operator.executing = operator.commandQueue.shift();
+      try {
+        operator.executing.ws.send(
+          ["S", operator.executing.id].join(" ").trim()
+        );
+      } catch (e) {
+        /* client stopped caring */
+      }
+
+      operator.telnet.exec(operator.executing.contents, (err, response) => {
+        let ws = operator.executing.ws;
+        let id = operator.executing.id;
+        operator.executing = null;
+        try {
+          ws.send(["O", id, response].join(" ").trim());
+          ws.send(["D", id].join(" ").trim());
+        } catch (e) {
+          /* client stopped caring */
+        }
+      });
+    }
+    setTimeout(syncCommunications, 20);
+  })();
+};
+
+
+
+helpers.isRunning("operator.exe", "myprocess", "myprocess").then(v => {
+  if (!v) {
+    helpers
+      .launchOperator(operatorPath, config.telnetPort, config.hide)
+      .then(
+        x =>
+          new Promise(resolve =>
+            setTimeout(() => resolve(x), config.telnetTimeout)
+          )
+      )
+      .then(operator => {
+        connectTelnet(operator, config.telnetPort);
+        if(config.autoOpen) opn("http://localhost");
+      })
+      .catch(reason => console.log(reason));
+  }
+});
+
+app.set( 'view engine', 'ejs' );
+app.use( express.static( './' ) );
+app.listen(config.webPort, () => {} );
---
+++ server/helpers.js
@@ -0,0 +1,116 @@
+const path = require("path");
+const spawn = require("child_process").spawn;
+const exec = require("child_process").exec;
+let Client = require("ssh2-sftp-client");
+let sftp = new Client();
+const config = require("./config.json");
+const fs = require("fs");
+
+// const credentials = require('./credentials.json');
+
+module.exports = {
+  isRunning: (win, mac, linux) => {
+    return new Promise(function(resolve, reject) {
+      const plat = process.platform;
+      const cmd =
+        plat == "win32"
+          ? "tasklist"
+          : plat == "darwin"
+          ? "ps -ax | grep " + mac
+          : plat == "linux"
+          ? "ps -A"
+          : "";
+      const proc =
+        plat == "win32"
+          ? win
+          : plat == "darwin"
+          ? mac
+          : plat == "linux"
+          ? linux
+          : "";
+      if (cmd === "" || proc === "") {
+        resolve(false);
+      }
+      exec(cmd, function(err, stdout, stderr) {
+        resolve(stdout.toLowerCase().indexOf(proc.toLowerCase()) > -1);
+      });
+    });
+  },
+  launchOperator: async (operatorPath, telnetPort, hide) => {
+    let spawnParams = { cwd: path.dirname(operatorPath) };
+    let operatorArgs = [
+      "--Telnet",
+      "*:" + telnetPort,
+      "--StartupIdle",
+      "--Title",
+      "--BCI2000Web"
+    ];
+    if (hide) operatorArgs.push("--Hide");
+    let operator = spawn(operatorPath, operatorArgs, spawnParams);
+    return operator;
+  },
+  connect2STFP: () => {
+    //docs
+    //https://www.npmjs.com/package/ssh2-sftp-client
+    //         var fs = require('fs');
+
+    // // ...
+
+    // for(var i = 0; i < data.length; i++) {
+    //   const remoteFilename = '/path/to/remote/files/' + data[i].name;
+    //   const localFilename = '/path/to/local/files/' + data[i].name;
+    //   sftp.get(remoteFilename).then((stream) => {
+    //     stream.pipe(fs.createWriteStream(localFilename));
+    //   });
+    // }
+    sftp
+      .connect({
+        host: "zappa.neuro.jhu.edu",
+        port: "22",
+        username: config.sftpUser,
+        password: credentials.sftpPass
+      })
+      .then(() => {
+        return sftp.list("/mnt/shared/ecog");
+      })
+      .then(data => {
+        console.log(data, "the data info");
+      })
+      .catch(err => {
+        console.log(err, "catch error");
+      });
+  },
+  findCards: currentDirPath => {
+    let cards = [];
+    fs.readdirSync(currentDirPath).forEach(name => {
+      const filePath = path.join(currentDirPath, name);
+      const stat = fs.statSync(filePath);
+      if (stat.isFile() && path.basename(filePath) == "cards.ejs") {
+        cards.push(path.resolve(filePath));
+      } else if (stat.isDirectory()) {
+        cards = cards.concat(module.exports.findCards(filePath));
+      }
+    });
+    return cards;
+  },
+
+  findData: currentDirPath => {
+    let data = [];
+    fs.readdirSync(currentDirPath).forEach(name => {
+      const filePath = path.join(currentDirPath, name);
+      const stat = fs.statSync(filePath);
+      if (stat.isFile() && path.extname(filePath) === ".dat") {
+        data.push(
+          path
+            .resolve(filePath)
+            .split(".")
+            .slice(0, -1)
+            .join(".")
+        );
+      } else if (stat.isDirectory()) {
+        data = data.concat(module.exports.findData(filePath));
+      }
+    });
+    return data;
+  }
+};
