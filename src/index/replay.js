97af21b285e36b1e0ff81e7a3d7ec66ae1e3ae76
Chris <17508521+TheBrainChain@users.noreply.github.com>
2019-09-23 10:18:57 -0400

    parms folder and selectable replay filter

--- src/index/replay.js
+++ src/index/replay.js
@@ -1,9 +1,10 @@
 export const launchSession = (patient, task, file) => {
   var datafile = `../data/${patient}/${task}/${file}.dat`;
-
+  let filter = 'SpectralSignalProcessingMod'
+  if (task == 'CCEPS') filter = 'EvokedPotentialFilter'
   var script = "Reset System; ";
   script += "Startup System localhost; ";
-  script += "Start executable SpectralSignalProcessingMod --local; ";
+  script += `Start executable ${filter} --local; `;
   script += "Start executable DummyApplication --local; ";
   script +=
     "Start executable FilePlayback --local --FileFormat=Null --PlaybackStates=1 --PlaybackFileName=" +
@@ -11,11 +12,11 @@ export const launchSession = (patient, task, file) => {
     "; ";
 
   script += "Wait for Connected; ";
-  script += "Load Parameterfile ../parms.ecog/SpectralSigProc.prm; ";
+  script += "Load Parameterfile ../parms/SpectralSigProc.prm; ";
 
+  script += "Set Parameter WSSourceServer *:20100; ";
   script += "Set Parameter WSSpectralOutputServer *:20203; ";
   script += "Set Parameter WSConnectorServer *:20323; ";
-  script += "Set Parameter WSSourceServer *:20100; ";
 
   // script += "Set Config; ";
   // script += "Wait for Resting; ";
