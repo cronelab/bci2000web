149b1bf5111490aad3fbaf3a844c39f517ff87a7
TheBrainChain <c.coogan2201@gmail.com>
2022-02-22 09:54:43 -0500

    temp remove bci2000web

--- apps/bci2000web/src/custom/CustomConfig.cmake
+++
@@ -1,4 +0,0 @@
-INCLUDE_EXTENSION( WSConnectorFilter "${BCI2000_SRC_DIR}/custom/WSConnectorFilter" )
-INCLUDE_EXTENSION( WSSourceFilter "${BCI2000_SRC_DIR}/custom/WSSourceFilter" )
-INCLUDE_EXTENSION( SerialOutputFilter "${BCI2000_SRC_DIR}/custom/SerialOutputFilter" )
-INCLUDE_EXTENSION( IMULogger "${BCI2000_SRC_DIR}/custom/IMULogger" )
